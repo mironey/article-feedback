387abb25f921f59c89007383a402425e856bc1f1
mohidul-islam <mironey45@gmail.com>
2026-03-09 23:40:48 +0600

    first commit

---
+++ article-feedback.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Plugin Name: Article Feedback 
+ * Description: Enable visitors to rate your posts or pages and leave feedback. Helps site owners measure content usefulness and gather insights to improve their articles.
+ * Plugin URI: https://example.com/article-feedback/
+ * Version: 1.0
+ * Author: Mohidul Islam
+ * Author URI: https://example.com/
+ * Requires at least: 5.0
+ * Requires PHP: 7.4
+ * License: GPL V2 or Later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-article-feedback
+ * Domain Path: /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
